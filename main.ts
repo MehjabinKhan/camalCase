af288b38ac0f9ad84a005afb45ec7d66aac00fc5
MehjabinKhan <161743992+MehjabinKhan@users.noreply.github.com>
2024-04-13 14:07:22 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,20 @@
+let message1 = "Hey";
+console.log(message1);
+message1 = "Hello";
+console.log(message1);
+
+// without camelCase
+let firstname1 = "Mehjabin";
+console.log(firstname1);
+console.log("everyonelikespizza");
+console.log("learningtypescript");
+console.log("easiertoread");
+console.log("codingthenewhobby");
+
+//Now using camelcase;
+let firstName = "Mehjabin";
+console.log(firstName);
+console.log("EveryoneLikesPizza");
+console.log("LearingTypescript");
+console.log("EasierToRead");
+console.log("CodingTheNewHobby");
